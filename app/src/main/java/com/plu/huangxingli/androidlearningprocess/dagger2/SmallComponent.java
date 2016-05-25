bb0ddde1afe1fb9b60e4048d018e92114080a757
Huangxingli <huangxingli@plu.cn>
2016-05-25 18:39:15 +0800

    【feature】注意貌似不可以有多个composement注入同一个Activity,若需要多个composement,则通过composement依赖的方式实现

--- app/src/main/java/com/plu/huangxingli/androidlearningprocess/dagger2/SmallComponent.java
+++ app/src/main/java/com/plu/huangxingli/androidlearningprocess/dagger2/SmallComponent.java
@@ -20,7 +20,7 @@ import dagger.Provides;
 @Component(modules = {SmallMoudle.class})
 public interface SmallComponent {
 
-    public Child provideChild1();
+    public Child provideChild1(); //注意此处这个方法一定不就可以少,因为该compenent被领外一个component依赖,在Activity中需要到SmallMoudle里面的Child,所以此处要主动提供
 
 
 
